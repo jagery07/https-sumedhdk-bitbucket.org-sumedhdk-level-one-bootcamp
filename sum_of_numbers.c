7840b812b13b88bdf204168a3c4b4f231292553c
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-02-12 13:51:46 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,15 +1,20 @@
 //Write a program to find the sum of n different numbers using 4 functions
 #include<stdio.h>
-#include<math.h>
 int main()
 {
-   int n,sum = 0,i,a[100];
-   scanf("%d", &n);
-   for (i= 0;i< n;i++)
-   {
-      scanf("%d", &a[i]);
-      sum = sum + a[i];
-   }
-   printf("the Sum = %d",sum);
-   return 0;
+ int i,n,arr[100], sum=0 ;
+ printf("Enter size of array");
+ scanf("%d",&n);
+ printf("Enter element to be added");
+ for(i=0;i<n;i++)
+ {
+  scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{ 
+ sum=sum+arr[i];
+}
+
+printf("Sum of numbers are:%d  \n",sum);
+
 }
