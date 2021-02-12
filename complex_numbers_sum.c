1653a95c811bac82aa00495b75aeb17ccf903cf5
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-02-12 14:58:42 +0530

    Update complex_numbers_sum.c

--- complex_numbers_sum.c
+++ complex_numbers_sum.c
@@ -1,1 +1,48 @@
-//WAP to find the sum of n complex numbers using structures and 4 or more functions.
+//Write a program to compute sum of n fractions using structures and functions.#include<stdio.h>
+#include<math.h>
+struct variables
+{
+    int x[10];
+    int y[10];
+};
+struct fraction
+{
+    double z[10];
+};
+int sum_fraction(struct variables var,int num);
+int main(void)
+{
+    struct variables var;
+    struct fraction fr;
+    int i,num;
+    printf("How many fractions you want?:\n");
+    scanf("%d",&num);
+    for(i=0;i<num;i++)
+        {
+            printf("Enter numerator %d: ",i);
+            scanf("%d",&var.x[i]);
+            printf("\n");
+            printf("Enter denominator %d: ",i);
+            scanf("%d",&var.y[i]);
+            printf("\n");
+        }
+    sum_fraction(var,num);
+    return 0;
+}
+
+int sum_fraction(struct variables var,int num)
+{
+    int i;
+    double add;
+    add=0;
+    struct fraction fr1;
+    for(i=0;i<num;i++)
+        {
+            fr1.z[i]=(double)var.x[i]/(double)var.y[i];
+        }
+    for(i=0;i<num;i++)
+    {
+        add+=fr1.z[i];
+    }
+    printf("\nSum of %d fractions is %lf",num,add);
+}
