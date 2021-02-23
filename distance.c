27262895ca512dc90ba30b85c1b8814f7cc17ad5
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-02-23 11:10:39 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,32 +1,40 @@
 //WAP to find the distance between two point using 4 functions.
 #include<stdio.h>
 #include<math.h>
-float fun1(float x1,float x2)
+
+void display(float a1, float b1,float a2, float b2);
+float points_distance(float *a1,float *b1, float *a2,float *b2);
+void output(float d);
+
+int main()
 {
- return (x2-x1)*(x2-x1);
+    float a1,b1,a2,b2;
+    float distance;
+    printf("Enter the coordinates for x1 and y1:\n");
+    scanf("%f %f",&a1,&b1);
+    printf("Enter the coordinates for x2 and y2:\n");
+    scanf("%f %f",&a2,&b2);
+    display(a1,b1,a2,b2);
+    distance = points_distance(&a1,&b1,&a2,&b2);
+    output(distance);
+    return 0;
 }
 
-float fun2(float y1,float y2)
+void display(float a1, float b1,float a2, float b2)
 {
- return (y2-y1)*(y2-y1); 
-}
- 
-float dist(float x1,float y1,float x2,float y2)
-{
- return  sqrt(fun1(x1,x2)+fun2(y1,y2));
+    printf("The coordinate of points are:\n");
+    printf("x1=%f, y1=%f\n",a1,b1);
+    printf("x2=%f, y2=%f\n",a2,b2);
 }
 
-float out(float x1,float x2, float y1, float y2)
+float points_distance(float *a1,float *b1, float *a2,float *b2)
 {
- printf("Distance between %f %f and %f %f is : %f\n",x1,x2,y1,y2,dist(x1,x2,y1,y2));
+    float d;
+    d = (sqrt)((*a2-*a1)(*a2-*a1)+(*b2-*b1)(*b2-*b1));
+    return d;
 }
 
-float main()
+void output(float d)
 {
- float x1,y1,x2,y2;
- printf("Enter coordinate of point 1:\n");
- scanf("%f%f",&x1,&y1);
- printf("Enter coordinate of point 2: \n");
- scanf("%f%f",&x2,&y2);
- out(x1,y1,x2,y2);
+    printf("Distance between the two points are %f\n",d);
 }
