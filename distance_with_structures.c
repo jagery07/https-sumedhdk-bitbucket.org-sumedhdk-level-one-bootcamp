33fa446175e86d06ecc471144c2897e2092efe0c
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-03-23 11:33:54 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -3,45 +3,45 @@
 #include<math.h>
 struct values
 {
-    float x1;
-    float y1;
-    float x2;
-    float y2;
+    float a1;
+    float b1;
+    float a2;
+    float b2;
 };
-struct values val;
-float input_values();
+
+struct values v;
+float input_val();
 double calc_dist();
-float output_values(double b);
+float output_val(double b);
 
 int main(void)
 {
-    //struct values val;
-    input_values();
+    input_val();
     calc_dist();
     return 0;
 }
 
-float input_values()
+float input_val()
 {
     printf("Enter x1: ");
-    scanf("%f",&val.x1);
+    scanf("%f",&v.a1);
     printf("\nEnter y1: ");
-    scanf("%f",&val.y1);
+    scanf("%f",&v.b1);
     printf("\nEnter x2: ");
-    scanf("%f",&val.x2);
+    scanf("%f",&v.a2);
     printf("\nEnter y2: ");
-    scanf("%f",&val.y2);
+    scanf("%f",&v.b2);
 }
 
 double calc_dist()
 {
-    double a,b;
-    a=((val.x2-val.x1)(val.x2-val.x1))+((val.y2-val.y1)(val.y2-val.y1));
-    b=sqrt(a);
-    output_values(b);
+    double x,y;
+    x=((v.a2-v.a1)(v.a2-v.a1))+((v.b2-v.b1)(v.b2-v.b1));
+    y=sqrt(x);
+    output_val(y);
 }
 
-float output_values(double b)
+float output_val(double y)
 {
-    printf("\nDistance between (%f,%f) and (%f,%f) is %lf\n",val.x1,val.y1,val.x2,val.y2,b);
+    printf("\nDistance b/w (%f,%f) and (%f,%f) is %lf\n",v.a1,v.b1,v.a2,v.b2,y);
 }
