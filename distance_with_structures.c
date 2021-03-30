0b232603d5249872356be28d3cfa15a667696321
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-03-30 16:33:29 +0530

    Update distance_with_structures.c

--- distance_with_structures.c
+++ distance_with_structures.c
@@ -1,47 +1,27 @@
 //WAP to find the distance between two points using structures and 4 functions.#include<stdio.h>
-#include<stdio.h>
-#include<math.h>
-struct values
-{
-    float a1;
-    float b1;
-    float a2;
-    float b2;
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct coordinates
+ {
+   float x, y;
 };
-
-struct values v;
-float input_val();
-double calc_dist();
-float output_val(double b);
-
-int main(void)
+double funcdist(struct coordinates a, struct coordinates b)
 {
-    input_val();
-    calc_dist();
+    float d;
+    d = sqrt((a.x - b.x) * (a.x - b.x) + (a.y-b.y) *(a.y-b.y));
+    return d;
+}
+int main()
+{
+    float d;
+    struct coordinates a, b;
+    printf("Enter coordinates of point a(x1,y1): ");
+    scanf("%f %f", &a.x, &a.y);
+    printf("Enter coordinates of point b(x2,y2): ");
+    scanf("%f %f", &b.x, &b.y);
+    d=funcdist(a, b);
+    printf("Distance between a and b: %f\n", d);
     return 0;
-}
-
-float input_val()
-{
-    printf("Enter x1: ");
-    scanf("%f",&v.a1);
-    printf("\nEnter y1: ");
-    scanf("%f",&v.b1);
-    printf("\nEnter x2: ");
-    scanf("%f",&v.a2);
-    printf("\nEnter y2: ");
-    scanf("%f",&v.b2);
-}
-
-double calc_dist()
-{
-    double x,y;
-    x=((v.a2-v.a1)(v.a2-v.a1))+((v.b2-v.b1)(v.b2-v.b1));
-    y=sqrt(x);
-    output_val(y);
-}
-
-float output_val(double y)
-{
-    printf("\nDistance b/w (%f,%f) and (%f,%f) is %lf\n",v.a1,v.b1,v.a2,v.b2,y);
 }
