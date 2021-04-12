ca24cc7aeeefbff021598e727d9a740281648dc5
jagery07 <78416671+jagery07@users.noreply.github.com>
2021-04-12 09:49:39 +0530

    Update complex_numbers_sum.c

--- complex_numbers_sum.c
+++ complex_numbers_sum.c
@@ -1,48 +1,87 @@
 //Write a program to compute sum of n fractions using structures and functions.#include<stdio.h>
-#include<math.h>
-struct variables
+#include<stdio.h>
+#include<stdio.h>
+struct fractions
 {
-    int x[10];
-    int y[10];
+    int num;
+    int den;
 };
-struct fraction
+
+void input_n_fractions(int *n, struct fractions answer[10]);
+struct fractions input_one_fraction(int n);
+struct fractions add_n_fractions(int n, struct fractions f1[10]);
+int great_com_den(int a,int b);
+void print_n_fractions(int n,struct fractions f2[10],struct fractions final_answer);
+
+int main()
 {
-    double z[10];
-};
-int sum_fraction(struct variables var,int num);
-int main(void)
-{
-    struct variables var;
-    struct fraction fr;
-    int i,num;
-    printf("How many fractions you want?:\n");
-    scanf("%d",&num);
-    for(i=0;i<num;i++)
-        {
-            printf("Enter numerator %d: ",i);
-            scanf("%d",&var.x[i]);
-            printf("\n");
-            printf("Enter denominator %d: ",i);
-            scanf("%d",&var.y[i]);
-            printf("\n");
-        }
-    sum_fraction(var,num);
-    return 0;
+    int i,n;
+    struct fractions input_values[10],add_fraction_result;
+
+    input_n_fractions(&n,input_values);
+    add_fraction_result=add_n_fractions(n,input_values);
+    print_n_fractions(n,input_values,add_fraction_result);
 }
 
-int sum_fraction(struct variables var,int num)
+void input_n_fractions(int *n, struct fractions answer[10])
 {
     int i;
-    double add;
-    add=0;
-    struct fraction fr1;
-    for(i=0;i<num;i++)
-        {
-            fr1.z[i]=(double)var.x[i]/(double)var.y[i];
-        }
-    for(i=0;i<num;i++)
+    printf("Enter no of fractions:\n");
+    scanf("%d",n);
+    for(i=0;i<*n;i++)
     {
-        add+=fr1.z[i];
+        answer[i]=input_one_fraction(i+1);
     }
-    printf("\nSum of %d fractions is %lf",num,add);
+}
+
+struct fractions input_one_fraction(int k)
+{
+    struct fractions f1;
+    printf("\nEnter numerator %d: ",k);
+    scanf("%d",&f1.num);
+    printf("\nEnter denominator %d: ",k);
+    scanf("%d",&f1.den);
+    return f1;
+}
+
+struct fractions add_n_fractions(int n, struct fractions f1[10])
+{
+    int i,gcd;
+    struct fractions ans;
+    ans.num=f1[0].num;
+    ans.den=f1[0].den;
+    for(i=1;i<n;i++)
+    {
+        ans.num=ans.num*f1[i].den+ans.den*f1[i].num;
+        ans.den=ans.den*f1[i].den;
+    }
+    gcd=great_com_den(ans.num,ans.den);
+    ans.num=ans.num/gcd;
+    ans.den=ans.den/gcd;
+    return ans;
+}
+
+int great_com_den(int a,int b)
+{
+    int i,gcd;
+    gcd=1;
+    for(i=2;i<=a && i<=b;i++)
+    {
+        if((a%i==0)&&(b%i==0))
+        {
+            gcd=i;
+        }
+    }
+    return gcd;
+}
+
+void print_n_fractions(int n,struct fractions f2[10],struct fractions final_answer)
+{
+    int i;
+    printf("%d/%d",f2[0].num,f2[0].den);
+    for(i=1;i<n;i++)
+    {
+        printf("+%d/%d",f2[i].num,f2[i].den);
+    }
+    printf("= %d/%d",final_answer.num,final_answer.den);
 }
